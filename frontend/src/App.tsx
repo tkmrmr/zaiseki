31c02736e3ea4a7bdcda449ac975b90b35d3105f
tkmrmr <dach1024@yahoo.co.jp>
2026-03-30 01:37:15 +0900

    add NotFound Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom"; // TODO: 可能であればBrowserRouterに変更
 import View from "@/pages/View";
-import Kiosk from "@/pages/Kiosk";
+import NotFound from "@/pages/NotFound";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<View />} />
-        <Route path="/kiosk" element={<Kiosk />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
