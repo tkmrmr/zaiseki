2949efb3f15d5934f429b1a43fbd93bc897ccf93
tkmrmr <dach1024@yahoo.co.jp>
2026-04-10 17:07:29 +0900

    bocco.pyのsent_message関数を追加

--- backend/cgi-bin/zaiseki/api/common/__init__.py
+++ backend/cgi-bin/zaiseki/api/common/__init__.py
@@ -1,5 +1,6 @@
+from .bocco import sent_message
 from .convert_to_utc_iso import convert_to_utc_iso
 from .get_db_connection import get_db_connection
 from .print_json import print_json
 
-__all__ = ["get_db_connection", "print_json", "convert_to_utc_iso"]
+__all__ = ["get_db_connection", "print_json", "convert_to_utc_iso", "sent_message"]
