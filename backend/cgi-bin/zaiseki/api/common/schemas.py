bf679124290bdc1168237a6db5b691f60aff38b9
tkmrmr <dach1024@yahoo.co.jp>
2026-04-17 01:29:37 +0900

    typeは3.12以降しか使えないので削除

--- backend/cgi-bin/zaiseki/api/common/schemas.py
+++ backend/cgi-bin/zaiseki/api/common/schemas.py
@@ -1,7 +1,7 @@
 from typing import Literal, NotRequired, TypedDict
 
-type SeatStatus = Literal["present", "absent", "vacant"]
-type Grade = Literal["B4", "M1", "M2", "D1", "D2", "D3"]
+SeatStatus = Literal["present", "absent", "vacant"]
+Grade = Literal["B4", "M1", "M2", "D1", "D2", "D3"]
 
 
 class Seat(TypedDict):
