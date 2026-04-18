24d4a57e3101a225868bc72e4c81c42044f18fb7
tkmrmr <dach1024@yahoo.co.jp>
2026-04-18 20:27:55 +0900

    日本語をエスケープせずに返す設定を追加

--- backend/cgi-bin/zaiseki/api/app.py
+++ backend/cgi-bin/zaiseki/api/app.py
@@ -5,3 +5,4 @@ from routes.public import bp as public_bp
 app = Flask(__name__)
 app.register_blueprint(public_bp)
 app.register_blueprint(kiosk_bp)
+app.config["JSON_AS_ASCII"] = False  # 日本語をエスケープせず返す
