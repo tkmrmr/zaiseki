1f23506db9adbb1a6ef66c24a713c9c772c8326a
tkmrmr <dach1024@yahoo.co.jp>
2026-04-18 19:52:06 +0900

    APIと被るためupdate_statusをupdate_seat_statusに変更

--- backend/cgi-bin/zaiseki/api/services/__init__.py
+++ backend/cgi-bin/zaiseki/api/services/__init__.py
@@ -1,10 +1,10 @@
-from .seats import list_full_status, list_public_status, update_status
+from .seats import list_full_status, list_public_status, update_seat_status
 from .students import assign_student_to_seat, list_students, unassign_student_from_seat
 
 __all__ = [
     "list_public_status",
     "list_full_status",
-    "update_status",
+    "update_seat_status",
     "list_students",
     "assign_student_to_seat",
     "unassign_student_from_seat",
