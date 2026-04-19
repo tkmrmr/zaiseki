707f4f96b071df3de8d0fabe0a41769a8757cce7
tkmrmr <dach1024@yahoo.co.jp>
2026-04-19 21:44:23 +0900

    configを分離

--- backend/cgi-bin/zaiseki/api/app/__init__.py
+++ backend/cgi-bin/zaiseki/api/app/__init__.py
@@ -1,35 +1,20 @@
-import os
-from pathlib import Path
-
-from dotenv import load_dotenv
 from flask import Flask
 from routes.admin import bp as admin_bp
 from routes.kiosk import bp as kiosk_bp
 from routes.public import bp as public_bp
 
+from .config import Config
 from .error_handler import register_error_handlers
-
-BASE_DIR = Path(__file__).resolve().parents[2]
-ENV_PATH = BASE_DIR / ".env"
-
-load_dotenv(ENV_PATH)
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config.from_object(Config)
 
-    app.config["DB_HOST"] = os.getenv("MARIADB_HOST", "db")
-    app.config["DB_USER"] = os.getenv("MARIADB_USER")
-    app.config["DB_PASSWORD"] = os.getenv("MARIADB_PASSWORD")
-    app.config["DB_NAME"] = os.getenv("MARIADB_DATABASE")
-    app.config["ENABLE_BOCCO"] = os.getenv("ENABLE_BOCCO", "false").lower() == "true"
-    app.config["BOCCO_REFRESH_TOKEN"] = os.getenv("BOCCO_REFRESH_TOKEN")
-    app.config["BOCCO_ROOM_ID"] = os.getenv("BOCCO_ROOM_ID")
+    register_error_handlers(app)
 
     app.register_blueprint(public_bp)
     app.register_blueprint(kiosk_bp)
     app.register_blueprint(admin_bp)
-    app.config["JSON_AS_ASCII"] = False  # 日本語をエスケープせず返す
-    register_error_handlers(app)
 
     return app
