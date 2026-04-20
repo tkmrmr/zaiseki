42315844358648236d06862d86cb2f22dad24d75
tkmrmr <dach1024@yahoo.co.jp>
2026-04-20 12:04:50 +0900

    不要な__all__定義を削除

--- backend/cgi-bin/zaiseki/api/app/utils/__init__.py
+++ backend/cgi-bin/zaiseki/api/app/utils/__init__.py
@@ -2,10 +2,3 @@ from .bocco import send_message
 from .convert_to_utc_iso import convert_to_utc_iso
 from .parse_request import parse_request
 from .validation import is_valid_positive_int
-
-__all__ = [
-    "convert_to_utc_iso",
-    "send_message",
-    "parse_request",
-    "is_valid_positive_int",
-]
