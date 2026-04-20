0550c23538ec1c6fd6cedd3236f1e91ed15604f9
tkmrmr <dach1024@yahoo.co.jp>
2026-04-20 11:04:09 +0900

    assignment_queriesへの置き換え

--- backend/cgi-bin/zaiseki/api/app/services/assignment.py
+++ backend/cgi-bin/zaiseki/api/app/services/assignment.py
@@ -1,14 +1,8 @@
-from app.db.queries import presence_queries, student_queries
+from app.db.queries import assignment_queries, presence_queries
 
 
 def assign_student_to_seat(student_id: int, seat_id: int) -> bool:
-    exists_student = student_queries.exists_student(student_id)
-    if not exists_student:
-        return False
-    presence_queries.delete_by_seat_id(seat_id)
-    presence_queries.delete_by_student_id(student_id)
-    presence_queries.create_presence_status(student_id, seat_id)
-    return True
+    return assignment_queries.assign_student_to_seat(student_id, seat_id)
 
 
 def unassign_student_from_seat(seat_id: int) -> None:
