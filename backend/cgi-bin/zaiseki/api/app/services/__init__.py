497ecceeba08f326dfe0b2e54f28dd034e448205
tkmrmr <dach1024@yahoo.co.jp>
2026-04-22 01:17:17 +0900

    import文を整理

--- backend/cgi-bin/zaiseki/api/app/services/__init__.py
+++ backend/cgi-bin/zaiseki/api/app/services/__init__.py
@@ -1,12 +1,1 @@
-from .assignment_service import assign_student_to_seat, unassign_student_from_seat
-from .status_service import list_full_status, list_public_status, update_seat_status
-from .student_service import list_students
 
-__all__ = [
-    "list_public_status",
-    "list_full_status",
-    "update_seat_status",
-    "list_students",
-    "assign_student_to_seat",
-    "unassign_student_from_seat",
-]
